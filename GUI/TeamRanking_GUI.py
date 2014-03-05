416172b16c8ec3ab63b189914a2bb268655b5f51
Aaron Wang <aaronw94@gmail.com>
2014-03-04 23:58:27 -0800

    attempted to fix the issue of values being passed as strings as opposed to integers. not successful. Need to find way to run function that takes several arguments without immediate execution

--- GUI/TeamRanking_GUI.py
+++ GUI/TeamRanking_GUI.py
@@ -26,14 +26,55 @@ class TeamRanking(Frame):
         shooting_ranking_list = aggregated_ranking_list[4]
         catches_ranking_list = aggregated_ranking_list[5]
         
-        team_cycle_ranking = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],] #TODO: Change the number of lists in this list per regional.
-        
+        team_cycle_ranking = []
         lb_cycle_ranking = Listbox(self.parent)
         
         for i in cycle_ranking_list:
-            team_cycle_ranking[i][1] = cycle_ranking_list[i][1]
-            lb_cycle_ranking.INSERT(END,team_cycle_ranking[i][1]) 
+            team_cycle_ranking[i] = cycle_ranking_list[i][1]
+            lb_cycle_ranking.INSERT(END,team_cycle_ranking[i]) 
+        lb_cycle_ranking.pack(side=LEFT, 
+                              padx=15, 
+                              pady=5)
         
+        team_assist_ranking = []
+        lb_assist_ranking = Listbox(self.parent)
+        
+        for j in assist_ranking_list:
+            team_assist_ranking[j] = assist_ranking_list[j][1]
+            lb_assist_ranking.INSERT(END,team_assist_ranking[j])
+        lb_assist_ranking.pack(side=LEFT,
+                               padx=15,
+                               pady=5)
+            
+        team_truss_ranking = []
+        lb_truss_ranking = Listbox(self.parent)
+        
+        for k in truss_ranking_list:
+            team_truss_ranking[k] = truss_ranking_list[k][1]
+            lb_truss_ranking.INSERT(END,team_truss_ranking[k])
+        lb_truss_ranking.pack(side=LEFT,
+                              padx=15,
+                              pady=5)
+        
+        team_shooting_ranking = []
+        lb_shooting_ranking = Listbox(self.parent)
+        
+        for l in shooting_ranking_list:
+            team_shooting_ranking[l] = shooting_ranking_list[l][1]
+            lb_shooting_ranking.INSERT(END,team_shooting_ranking[l])
+        lb_shooting_ranking.pack(side=LEFT,
+                                 padx=15,
+                                 pady=5)
+        
+        team_catches_ranking = []
+        lb_catches_ranking = Listbox(self.parent)
+        
+        for m in catches_ranking_list:
+            team_catches_ranking[m] = catches_ranking_list[m][1]
+            lb_catches_ranking.INSERT(END,team_catches_ranking[m])
+        lb_catches_ranking.pack(side=LEFT,
+                                 padx=15,
+                                 pady=5)
         
 def Team_Ranking():
     root_team_ranking = Tk()
