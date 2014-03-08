b0fe30efca3f5ed7acb7def9277341f8d8c34646
Aaron Wang <aaronw94@gmail.com>
2014-03-07 19:29:10 -0800

    TeamLookup WIP

--- GUI/TeamLookup_GUI.py
+++ GUI/TeamLookup_GUI.py
@@ -4,7 +4,8 @@ Created on Feb 17, 2014
 @author: Aaron "Freshman" Wang
 '''
 from Tkinter import *
-from ttk import Frame, Button, Style
+from ttk import *
+import BackEnd.DataHandler
 
 class TeamLookup(Frame):
     def __init__(self,parent):
@@ -16,11 +17,23 @@ class TeamLookup(Frame):
         self.style = Style()
         self.style.theme_use("default")
         
+        match_performance_history =  BackEnd.DataHandler.getTeam()
         
-    
+        shooting_performance_list = match_performance_history["shooting"]
+        shooting_performance_h_list = shooting_performance_list[1]
+        
+        
+        assist_performance_list = match_performance_history["assist"]
+        
+        truss_performance_list = match_performance_history["truss"]
+        
+        auton_performance_list = match_performance_history["auton"]
+        
 def Team_Lookup():
     root_team_lookup = Tk()
     root_team_lookup.geometry("750x650+300+300")
     app_team_lookup = TeamLookup(root_team_lookup)
     root_team_lookup.mainloop()
     
+if __name__ == "__main__":
+    Team_Lookup()
